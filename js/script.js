93d06a182f98c15d3c0175cc83f02ee68c157969
Shadekul Shovo <shadekuljamalpur.bd21@gmail.com>
2022-03-02 06:53:03 +0600

    Main Section

--- js/script.js
+++ js/script.js
@@ -14,3 +14,33 @@ const searchPhone = () => {
     .then((response) => response.json())
     .then((data) => displaySearchResult(data.data));
 };
+
+// Main
+
+const displaySearchResult = (data) => {
+  console.log(data);
+  const searchResult = document.getElementById("searchResult");
+
+  data.forEach((phoneData) => {
+    console.log(phoneData);
+    const div = document.createElement("div");
+    div.innerHTML = `
+            
+            <div class="col">
+            <div class="card h-100">
+              <img src="${phoneData.image}" class="card-img-top w-50 p-3 rounded mx-auto d-block" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Name: ${phoneData.phone_name}</h5>
+                <h6>Brand: ${phoneData.brand}</h6>
+                <div>
+                <button onclick="loadPhoneName('${phoneData.slug}')" type="button" class="btn btn-primary">Show Details</button>
+                
+                </div>
+    
+              </div>
+            </div>
+          </div>
+            `;
+    searchResult.appendChild(div);
+  });
+};
